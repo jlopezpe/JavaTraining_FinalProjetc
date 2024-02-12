3d50c7edcd9537d046756250c29ebb9b41dbbea3
jonathan.lp <jlopezpe@unal.edu.co>
2024-02-11 22:38:34 -0500

    creation of getters and setters of PartTime_Teacher

--- TAE_FinalProject/src/PartTime_Teacher.java
+++ TAE_FinalProject/src/PartTime_Teacher.java
@@ -3,6 +3,22 @@ public class PartTime_Teacher extends Person implements Teacher {
     private int hours_week;
     private double salary;
 
+    public int getHours_week() {
+        return hours_week;
+    }
+
+    public void setHours_week(int hours_week) {
+        this.hours_week = hours_week;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
     public void calculate_salary() {
 
     }
