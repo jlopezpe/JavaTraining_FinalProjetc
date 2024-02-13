a199d19b84c1a53b65b0959762fd7b9364e3b19a
jonathan.lp <jlopezpe@unal.edu.co>
2024-02-13 16:26:30 -0500

    creating user interface

--- TAE_FinalProject/src/Main.java
+++ TAE_FinalProject/src/Main.java
@@ -1,14 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        Menu.Begin();
-        //university.show_professorsInfo();
-        //university.showinfo_student();
-        //university.show_classesInfo(university.getClasses());
-       //university.show_classes_menu(university.getClasses());
-        //university.create_student();
-        //university.search_Studentclass();
-        //university.create_class();
+        //Menu.Begin();
+        App_Interface.InterfazPrincipal ventanaPrincipal = new App_Interface.InterfazPrincipal();
+        ventanaPrincipal.setVisible(true);
 
     }
 
